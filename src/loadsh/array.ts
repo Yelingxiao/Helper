a8bb048487bffd4512b5d867bec39978307c5525
yelingxiao <1003772257@qq.com>
2019-09-03 17:41:24 +0800

    loadsh

--- src/loadsh/array.ts
+++ src/loadsh/array.ts
@@ -5,7 +5,7 @@
  * @Last Modified time: 2019-08-22 16:25:52
  * loadsh 4.17.15
  */
-export function chunk(array: Array, size: Number = 1): Array {
+export function chunk(array: array, size: number = 1): array {
   let newArray = []
   let nowArray = []
   array.forEach((item, index) => {
@@ -19,31 +19,53 @@ export function chunk(array: Array, size: Number = 1): Array {
   return newArray
 }
 
-export function compact(array: Array): Array {
+export function compact(array: array): array {
   return array.filter(item => item)
 }
 
-export function concat(...theArgs: any): Array {
+export function concat(...theArgs: any): array {
   return theArgs.map(item => {
     return Array.isArray(item) ? Number(item.join(',')) : item
   })
 }
 
-export function difference(inspect: Array, exclude: Array): Array {
+export function difference(inspect: array, exclude: Array): array {
   return inspect.filter(item => !exclude.includes(item))
 }
 
-export function drop(array: Array, n: Number = 1): Array {
+export function drop(array: array, n: number = 1): array {
   for (let i = 0; i < n; i++) {
     array.shift()
   }
   return array
 }
 
-export function dropRight(array: Array, n: Number = 1): Array {
+export function dropRight(array: array, n: number = 1): array {
   for (let i = 0; i < n; i++) {
     array.pop()
   }
   return array
 }
-console.log(dropRight([1, 2, 3], 5))
+
+export function dropRightWhile(array: array, n: Function): array {
+  for (let i = 0; i < n; i++) {
+    array.pop()
+  }
+  return array
+}
+
+export function fill(array: array, value: any, start: number = 0, end: number = array.length): array {
+  for (let i = start; i < end; i++) array[i] = value
+  return array
+}
+
+export function fromPairs(pairs: array): object {
+  let obj = {}
+  pairs.forEach(item => { obj[item[0]] = item[1] })
+  return obj
+}
+
+export function head(array: array): any {
+  return array[0]
+}
+console.log(head([]))
